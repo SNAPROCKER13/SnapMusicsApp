d19234ff7573b06ac3d7bc7b3de7dd1d5ae61985
Snaprocker <theerawat.s@trinitech.co.th>
2024-03-11 02:36:21 +0700

    update

--- src/App.js
+++ src/App.js
@@ -7,17 +7,18 @@ import Home from './Pages/Home'
 import Login from './Pages/Login'
 import Register from './Pages/Register'
 import Profile from './Pages/Profile'
-// import NotFound from './Pages/NotFound';
+import NotFound from './Pages/NotFound';
 
 function App() {
   return (
     <div className='bg-neutral-300'>
       <BrowserRouter> 
         <Routes>
-          <Route path="/" element={<Login/>} /> 
-          <Route path="/home" element={<Home/>} />
-          <Route path="/profile" element={<Profile/>} />
-          {/* <Route path="*" element={<NotFound />} /> */}
+          <Route path="/" element={<Login/>} >
+            <Route path="/home" element={<Home/>} />
+            <Route path="/profile" element={<Profile/>} />
+          </Route>  
+          <Route path="*" element={<NotFound />} />
           <Route path="/register" element={<Register/>} />     
         </Routes>
      </BrowserRouter>
