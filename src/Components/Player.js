c0a521954f548c48de921b704164a20a2dc8aa38
Snaprocker <theerawat.s@trinitech.co.th>
2024-03-11 11:29:10 +0700

    update

--- src/Components/Player.js
+++ src/Components/Player.js
@@ -1,7 +1,21 @@
 const Player = () => {
     return(
-        <div className="">
-            <p></p>
+        <div className="px-14 flex items-center justify-between">
+            <div>
+                <div className="w-24 h-24 bg-sky-400"></div>
+            </div>
+
+            <div className="flex w-1/4 justify-between">
+                <div>Previous</div>
+                <div>Range</div>
+                <div>Play</div>
+                <div>Next</div>
+            </div>
+
+            <div className="flex w-1/4 justify-around">
+                <p>Title : {}</p>
+                <p>Artist : {}</p>
+            </div>
         </div>
     )
 }
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,16 +1,20 @@
 import Header from '../Components/Header'
 import Content from '../Components/Content'
+import Player from '../Components/Player'
 
 const Home = () => {
 
     return(
         <div className="h-full flex flex-col items-center">
             <Header/>
-            <div className='my-12'>
-                <input className="border-2 border-black-300 rounded-lg border-2  mx-12" placeholder='ค้นหา'/>
+            <div className='my-12 w-full text-center'>
+                <input className="border-2 border-black-300 rounded-lg border-2 mx-6 w-6/12" placeholder='  ค้นหา'/>
                 <button className='border-2 border-black-300 rounded-lg bg-sky-400 text-white border-2 '>ค้นหา</button>
             </div>
             <Content/>
+            <div className='bg-white w-full border-2 border-black'>
+                <Player/>
+            </div>
         </div>
     )
 
