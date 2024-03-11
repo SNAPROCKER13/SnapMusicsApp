60c922455e275233ff010d7a749641177fee63d4
Snaprocker <theerawat.s@trinitech.co.th>
2024-03-11 07:03:48 +0700

    update

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -10,7 +10,7 @@ const Header = () => {
                 <h1 className="text-3xl text-sky-400 font-bold">SNAPMUSICS</h1>
             </div>
             <div className="flex flex-row justify-center items-center">
-                {location.pathname != '/profile' ? <Link to="/profile"><div className="bg-white rounded-full my-14 w-16 h-16 flex justify-center items-center border-2 border-black mx-6">Profile</div></Link>: <button onClick={() => navigate(-1)} className="border-2 border-black rounded-lg h-max w-max text-black mx-6">Back</button>}
+                {location.pathname !== '/profile' ? <Link to="/profile"><div className="bg-white rounded-full my-14 w-16 h-16 flex justify-center items-center border-2 border-black mx-6">Profile</div></Link>: <button onClick={() => navigate(-1)} className="border-2 border-black rounded-lg h-max w-max text-black mx-6">Back</button>}
                 <button onClick={() => navigate('/')}className="border-2 bg-sky-400 rounded-lg h-max w-max text-white">Log Out</button>
             </div>
         </div>
